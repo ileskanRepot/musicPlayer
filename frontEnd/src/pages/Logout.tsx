e44c9b902c5a3865efe78ba93ba9d3e0bcdc4fe0
Ileska <ileska@tutanota.com>
2023-09-02 22:29:27 +0300

    Add logout to frontend

--- frontEnd/src/pages/Logout.tsx
+++ frontEnd/src/pages/Logout.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import settings from "../constants";
+import { Link, redirect } from "react-router-dom";
 
 const Logout = () => {
-  return <h1>Logout here</h1>;
+  fetch(`${settings.backendUrl}/api/logout`, {
+    credentials: "include",
+  }).then((_) => redirect("/"));
+  return (
+    <h1>
+      <Link to="/">HOME</Link>
+    </h1>
+  );
 };
 
 export default Logout;
